11e26b77692919723d9d2e1d2347cc3918a14e51
Weronika Rudnicka <wrudnicka@wikia-inc.com>
2020-07-21 09:08:05 +0200

    IW-3518 | some refactoring

--- src/themes/fandom/FandomTheme.js
+++ src/themes/fandom/FandomTheme.js
@@ -13,13 +13,13 @@ OO.inheritClass( OO.ui.FandomTheme, OO.ui.Theme );
 OO.ui.theme = new OO.ui.FandomTheme();
 
 $( function () {
-	var $globalNavigation = $( '.wds-global-navigation-wrapper' );
-	var wikiaBarWrapper = $( '#WikiaBarWrapper' );
-
 	OO.ui.getViewportSpacing = function () {
+		var $globalNavigation = $( '.wds-global-navigation-wrapper' );
+		var $wikiaBarWrapper = $( '#WikiaBarWrapper' );
 		var $summaryPanel = $( '.ve-ui-summaryPanel' );
+
 		var viewportTopOffset = $globalNavigation.height() || 0;
-		var viewportBottomOffset = !wikiaBarWrapper.hasClass( 'hidden' ) ? wikiaBarWrapper.height() || 0 : 0;
+		var viewportBottomOffset = !$wikiaBarWrapper.hasClass( 'hidden' ) ? $wikiaBarWrapper.height() || 0 : 0;
 
 		viewportBottomOffset += $summaryPanel.length ? $summaryPanel.outerHeight() : 0;
 
