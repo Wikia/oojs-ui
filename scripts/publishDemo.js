446f8080fbc151000630e64f19dc27faa1dbc2e5
Weronika Rudnicka <wrudnicka@fandom.com>
2021-01-26 11:04:57 +0100

    commit message

--- scripts/publishDemo.js
+++ scripts/publishDemo.js
@@ -1,16 +1,20 @@
+const fs = require('fs');
 const execSync = require('child_process').execSync;
-const ghpages = require('gh-pages');
+const ghPages = require('gh-pages');
+
+const pkg = JSON.parse(fs.readFileSync('./package.json'));
 
 // first update 'demos' directory
 execSync('grunt clean:demos && grunt copy:demos', { stdio: [0, 1, 2] });
 
-ghpages.publish(
+ghPages.publish(
 	'demos',
 	{
 		add: true,
 		dotfiles: false,
+		message: 'OOUI demo pages update: ' + pkg.version,
 		// for testing purposes
-		push: false,
+		// push: false,
 		async beforeAdd(git) {
 			return git.rm([
 				'./php',
