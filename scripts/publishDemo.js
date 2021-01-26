98a7bf73ac1992c1a03d77063388234cf43943ee
Weronika Rudnicka <wrudnicka@fandom.com>
2021-01-26 10:05:54 +0100

    ghpages call

---
+++ scripts/publishDemo.js
@@ -0,0 +1,18 @@
+const execSync = require('child_process').execSync;
+const ghpages = require('gh-pages');
+
+// first update 'demos' directory
+execSync('grunt clean:demos && grunt copy:demos', { stdio: [0, 1, 2] });
+
+ghpages.publish(
+	'demos',
+	{
+		add: true,
+		dotfiles: false,
+		// for testing purposes
+		push: false,
+		async beforeAdd(git) {
+			return git.rm('./php ./vendor ./composer.json ./composer.lock ./infusion.js ./*.php');
+		}
+	}
+);
